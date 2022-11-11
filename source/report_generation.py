9855f00f2c5899130e9279aa75d4193bff2531e7
GiannosL <giannos.louloudis@gmail.com>
2022-11-11 13:28:01 +0100

    creating empty html files for all tabs

--- source/report_generation.py
+++ source/report_generation.py
@@ -13,12 +13,38 @@ def edit_main_file(working_directory):
     main_file = read_html(template_name="source/templates/main.html")
     main_file = main_file.replace("_TRAIN_PC1_PC2_PATH_", f"{working_directory}training_data_pcs.png")
     main_file = main_file.replace("_TEST_PC1_PC2_PATH_", f"{working_directory}test_data_pcs.png")
+    
+    main_file = main_file.replace("main.html", "main_report.html")
+    main_file = main_file.replace("model.html", "model_report.html")
+    main_file = main_file.replace("results.html", "results_report.html")
     return main_file
+
+
+def edit_model_file():
+    model_file = read_html(template_name="source/templates/model.html")
+
+    model_file = model_file.replace("main.html", "main_report.html")
+    model_file = model_file.replace("model.html", "model_report.html")
+    model_file = model_file.replace("results.html", "results_report.html")
+    return model_file
+
+
+def edit_results_file():
+    results_file = read_html(template_name="source/templates/results.html")
+
+    results_file = results_file.replace("main.html", "main_report.html")
+    results_file = results_file.replace("model.html", "model_report.html")
+    results_file = results_file.replace("results.html", "results_report.html")
+    return results_file
 
 
 def make_report(working_directory):
 
     main_file = edit_main_file(working_directory)
+    model_file = edit_model_file()
+    results_file = edit_results_file()
 
     #
-    save_html(f"{working_directory}main_report.html", main_file)
+    save_html(f"{working_directory}report/main_report.html", main_file)
+    save_html(f"{working_directory}report/model_report.html", model_file)
+    save_html(f"{working_directory}report/results_report.html", results_file)
