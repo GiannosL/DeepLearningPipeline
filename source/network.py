f528bac6ce5c9ebf49acbacaa1721206d219c239
GiannosL <giannos.louloudis@gmail.com>
2022-11-11 12:58:06 +0100

    temporary - removed two out of three hidden layers

--- source/network.py
+++ source/network.py
@@ -7,7 +7,7 @@ class ANN(nn.Module):
 
     def __init__(self, 
                  in_features=4, 
-                 h1 = 12, h2 = 12, h3 = 12,
+                 h1 = 12,
                  out_features=3,
                  name="Pythagoras"):
 
@@ -18,16 +18,12 @@ class ANN(nn.Module):
         #
         self.in_features = in_features
         self.h1 = h1
-        self.h2 = h2
-        self.h3 = h3
         self.out_features = out_features
 
         # layers: input=4 -> h1 -> h2 N --> output=3
         # input - h1
         self.fc1 = nn.Linear(in_features, h1)
-        self.fc2 = nn.Linear(h1, h2)
-        self.fc3 = nn.Linear(h2, h3)
-        self.out = nn.Linear(h3, out_features)
+        self.out = nn.Linear(h1, out_features)
     
     def model_description(self):
         print("\n-------------------------------------------------------------------")
@@ -35,8 +31,6 @@ class ANN(nn.Module):
         print("Total number of layers = 5")
         print(f"Input layer:\t{self.in_features} nodes")
         print(f"Hidden layer 1:\t{self.h1} nodes")
-        print(f"Hidden layer 2:\t{self.h2} nodes")
-        print(f"Hidden layer 3:\t{self.h3} nodes")
         print(f"Output layer:\t{self.out_features} nodes")
 
         print("All layers are using the Rectified Linear Unit activation function.")
@@ -44,8 +38,6 @@ class ANN(nn.Module):
     
     def forward(self, x):
         x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
         x = self.out(x)
 
         return x
