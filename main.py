09d3994c2f13dc6a2a013642e8d36f0a10a799b1
GiannosL <giannos.louloudis@gmail.com>
2022-11-11 13:01:56 +0100

    added hpo for some variables

--- main.py
+++ main.py
@@ -1,20 +1,13 @@
-import os
-import yaml
 from source.network import ANN
 import source.model_preparation as mp
-from source.data_preparation import generate_datasets, standardize_data
+from source.setup.setup import setup_run
 from source.report_generation import make_report
+from source.hpo.hyper_param_opt import Hyper_Parameter_Optimization
+from source.data_preparation import generate_datasets, standardize_data
+
 
 # read configuration
-with open("model_configuration.yaml", "r") as f:
-    config = yaml.full_load(f)
-
-# create output
-try:
-    os.mkdir(config["work_directory"])
-    print(f"\n[ ] Creating \"{config['work_directory']}\"!\n[ ] Proceeding to read input data...")
-except:
-    print(f"\n[x] Directory \"{config['work_directory']}\" already exists!\n[ ] Proceeding to read input data...")
+config = setup_run("model_configuration.yaml") 
 
 # build datasets
 training_set, test_set = generate_datasets(feature_list=config["features"].split(","))
@@ -24,8 +17,15 @@ print("\n[ ] Calculationg PCs of input dataset.\n")
 training_set.plot_principal_components(save_path=config["work_directory"])
 test_set.plot_principal_components(save_path=config["work_directory"], train_test_flag="test")
 
+# perform hyper parameter optimization
+hpo = Hyper_Parameter_Optimization(training_set, n_trials=100)
+hpo.data.show_results()
+
 # start up main model
-model = ANN(in_features=training_set.feature_number, out_features=training_set.class_number)
+model = ANN(in_features=training_set.feature_number, 
+            h1=hpo.data.param_set["n_units"], 
+            out_features=training_set.class_number)
+
 # give model description
 model.model_description()
 
@@ -34,7 +34,8 @@ training_set, test_set = standardize_data(training_set, test_set)
 
 # train model, use standardized data
 trained_model, loss_history = mp.train_model(X=training_set.feature_matrix, y=training_set.target, 
-                                             model=model, epochs=200, plot_loss=True,
+                                             model=model, epochs=200, plot_loss=True, 
+                                             itta=hpo.data.param_set["learning_rate"],
                                              plot_name=f"{config['work_directory']}training_loss.png")
 
 # run on test set
