a9f5f1b19478ec9fc39b9ff4a1d0f79d2e162702
GiannosL <giannos.louloudis@gmail.com>
2022-11-11 13:28:32 +0100

    added to the output file-structure

--- source/setup/setup.py
+++ source/setup/setup.py
@@ -7,17 +7,24 @@ def read_configuration(filename):
     return config
 
 
+def create_directories(work_dir):
+    try:
+        os.mkdir(work_dir)
+        print(f"\n[ ] Creating \"{work_dir}\"!\n[ ] Proceeding to read input data...")
+    except:
+        print(f"\n[x] Directory \"{work_dir}\" already exists!")
+        print("[ ] Proceeding to read input data...")
+
+
 def setup_run(config_file):
 
     # get input files
     config = read_configuration(config_file)
 
     # set-up file structure
-    try:
-        os.mkdir(config["work_directory"])
-        print(f"\n[ ] Creating \"{config['work_directory']}\"!\n[ ] Proceeding to read input data...")
-    except:
-        print(f"\n[x] Directory \"{config['work_directory']}\" already exists!")
-        print("[ ] Proceeding to read input data...")
+    create_directories(config["work_directory"])
+    create_directories(f"{config['work_directory']}/report/")
+    create_directories(f"{config['work_directory']}/results/")
+    create_directories(f"{config['work_directory']}/results/plots/")
     
     return config
