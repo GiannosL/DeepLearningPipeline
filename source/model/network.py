a1a0c06c7d1007ece8665be7964a7e7adeb33bb2
GiannosL <giannos.louloudis@gmail.com>
2022-11-14 11:56:26 +0100

    restructured model away from functional

--- source/model/network.py
+++ source/model/network.py
@@ -5,66 +5,74 @@ import torch.nn.functional as F
 
 class ANN(nn.Module):
 
-    def __init__(self, 
-                 h1, h2, h3,
-                 epochs=50,
-                 in_features=4, 
-                 out_features=3,
-                 learning_rate=0.01,
-                 name="Pythagoras",
-                 training_flag=False,
-                 dropout_rate=0):
+    def __init__(self, name="Pythagoras"):
 
         super().__init__()
         # Model name
         self.name = name
 
-        # parameters that will be used for training the model
-        self.learning_rate = learning_rate
-        self.epochs = epochs
-        self.training_set_size = None
-        self.training_loss = None
-
-        # for dropout
-        self.training_flag = training_flag
-
         #
-        self.in_features = in_features
-        self.h1 = h1
-        self.h2 = h2
-        self.h3 = h3
-        self.out_features = out_features
-        self.dropout_rate = dropout_rate
+        self.setup_training()
 
-        # layers: input=4 -> h1 -> h2 N --> output=3
-        # input - h1
-        self.fc1 = nn.Linear(in_features, h1)
-        self.fc2 = nn.Linear(h1, h2)
-        self.fc3 = nn.Linear(h2, h3)
-        self.out = nn.Linear(h3, out_features)
+        # model
+        self.model = self.setup_model()
     
     def model_description(self):
         print("\n-------------------------------------------------------------------")
         print("Multi-layer perceptron model.")
         print("Total number of layers = 5")
         print(f"Input layer:\t{self.in_features} nodes")
-        print(f"Hidden layer 1:\t{self.h1} nodes")
-        print(f"Hidden layer 1:\t{self.h2} nodes")
-        print(f"Hidden layer 1:\t{self.h3} nodes")
+        print(f"Hidden layer 1:\t{self.h1_nodes} nodes")
+        print(f"Hidden layer 2:\t{'-'} nodes")
+        print(f"Hidden layer 3:\t{'-'} nodes")
         print(f"Output layer:\t{self.out_features} nodes")
 
         print("All layers are using the Rectified Linear Unit activation function.")
         print("-------------------------------------------------------------------\n")
     
-    def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, self.dropout_rate, training=self.training_flag)
-        x = F.relu(self.fc2(x))
-        x = F.dropout(x, self.dropout_rate, training=self.training_flag)
-        x = F.relu(self.fc3(x))
-        x = self.out(x)
+    def train_model(self, X, y_true):
+        losses = []
+        for epoch in self.epochs:
+            #
+            y_pred = self.model(X)
 
-        return x
+            #
+            loss = self.loss_function(y_pred, y_true)
+            losses.append(loss)
+
+            #
+            self.model.zero_grad()
+            loss.backward()
+
+            #
+            self.optimizer.step()
+    
+    def setup_training(self, loss_func=nn.MSELoss(), 
+                       optimizer=torch.optim.SGD(), epochs=1000):
+        self.loss_function = loss_func
+        self.optimizer = optimizer
+        self.epochs = epochs
+
+    def setup_model(self, input_layer_nodes, hidden_layer_nodes, output_layer_nodes):        
+        #
+        self.in_features = input_layer_nodes
+        self.h1_nodes = hidden_layer_nodes
+        self.out_features = output_layer_nodes
+        
+        #
+        model = nn.Sequential(
+            nn.Linear(self.in_features, self.h1_nodes),
+            nn.ReLU(),
+            nn.Linear(self.h1_nodes, self.out_features)
+        )
+
+        return model
+    
+    def predict(self, X):
+        with torch.no_grad:
+            y_pred = self.model(X)
+        
+        return y_pred
     
     def save(self, name):
         torch.save(self.state_dict(), name)
