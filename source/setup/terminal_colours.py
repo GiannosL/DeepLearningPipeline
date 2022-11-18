1a6ff94765b0a5ad910bdb9987017588cfde3201
GiannosL <giannos.louloudis@gmail.com>
2022-11-18 08:13:58 +0100

    object for colouring the standard output

---
+++ source/setup/terminal_colours.py
@@ -0,0 +1,10 @@
+class terminal_colors:
+    header = '\033[95m'
+    okblue = '\033[94m'
+    okcyan = '\033[96m'
+    okgreen = '\033[92m'
+    warning = '\033[93m'
+    fail = '\033[91m'
+    endc = '\033[0m'
+    bold = '\033[1m'
+    underline = '\033[4m'
